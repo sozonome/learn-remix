1fe23ccf880417543f2fd5f82fbbc4633ba5b57a
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-11-23 18:38:20 +0700

    feat: add post route

---
+++ app/post.ts
@@ -0,0 +1,19 @@
+export type Post = {
+  slug: string;
+  title: string;
+};
+
+export const getPosts = () => {
+  let posts: Array<Post> = [
+    {
+      slug: "my-first-post",
+      title: "My First Post",
+    },
+    {
+      slug: "90s-mixtape",
+      title: "A Mixtape I Made Just For You",
+    },
+  ];
+
+  return posts;
+};
